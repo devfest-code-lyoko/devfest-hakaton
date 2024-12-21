92f577660ff3aab53bf9c5c650e4389e07707117
tamarara02 <tamararadojcic02@gmail.com>
2024-12-21 18:22:52 +0100

    tamarino sve

---
+++ sve/srcfront/app/services/helper.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Service } from '../models/Service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+  http = inject(HttpClient)
+  backPath = "http://localhost:8080"
+  constructor() { }
+
+  getAllServices(){
+    return this.http.get<Service[]>(`${this.backPath}/getAllServices`)
+  }
+
+  chooseService(id: number){
+    const data={
+      id: id
+    }
+    return this.http.post<Service>(`${this.backPath}/getService`, data)
+  }
+}
