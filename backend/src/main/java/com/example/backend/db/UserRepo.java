84fbf6443e7a6d2de5974815fdde58861e5c5a0f
MiljanVojvodic <148571337+MiljanVojvodic@users.noreply.github.com>
2024-12-21 13:15:12 +0100

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/db/UserRepo.java
@@ -0,0 +1,72 @@
+package com.example.backend.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.example.backend.models.User;
+
+public class UserRepo implements UserRepoInterface {
+
+    @Override
+    public User login(User user) {
+        try(Connection conn = DB.source().getConnection();
+            PreparedStatement stmt = conn.prepareStatement("select * from users where username = ? and password = ? and type = ?")) {
+                stmt.setString(1, user.getUsername());
+                stmt.setString(2, user.getPassword());
+                stmt.setString(3, user.getType());
+
+                ResultSet rs = stmt.executeQuery();
+                if(rs.next()){
+                    return new User(
+                        rs.getString("firstname"),
+                        rs.getString("lastname"),
+                        rs.getString("password"),
+                        rs.getString("username"),
+                        rs.getString("type"),
+                        rs.getString("address"),
+                        rs.getString("subscription"),
+                        rs.getString("mail"));
+                }
+                
+             } catch(SQLException e){
+                e.printStackTrace();
+             }
+
+             return null;
+
+    }
+
+    @Override
+    public User register(User user) {
+        try(Connection conn = DB.source().getConnection();
+            PreparedStatement stmt = conn.prepareStatement("select * from users where username = ?")) {
+                stmt.setString(1, user.getUsername());
+                ResultSet res = stmt.executeQuery();
+                if(!res.next()){
+                    try(PreparedStatement stm = conn.prepareStatement("INSERT INTO users (firstname, lastname, password, username, type, address, subscription, mail) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")){
+
+                        stm.setString(1, user.getFirstname());
+                        stm.setString(2, user.getLastname());
+                        stm.setString(3, user.getPassword());
+                        stm.setString(4, user.getUsername());
+                        stm.setString(5, user.getType());
+                        stm.setString(6, user.getAddress());
+                        stm.setString(7, user.getSubscription());
+                        stm.setString(8, user.getMail());
+
+                        stm.executeUpdate();
+                        return user;
+                    } catch(SQLException e){
+                        e.printStackTrace();
+                    }
+                }  
+            }
+
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/db/UserRepoInterface.java
@@ -0,0 +1,8 @@
+package com.example.backend.db;
+
+import com.example.backend.models.User;
+
+public interface UserRepoInterface {
+    public User login(User user);
+    public User register(User user);
+}
